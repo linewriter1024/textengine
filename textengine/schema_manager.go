2cb1ad500253f3066dc64baba2553cf00f94fcd8
Benjamin Leskey <ben@benleskey.com>
2023-05-01 13:51:38 -0400

    Implement schema manager version methods

--- textengine/schema_manager.go
+++ textengine/schema_manager.go
@@ -1,13 +1,17 @@
 package textengine
 
 func (game *Game) CreateSchemaTableIfNeeded() error {
-	_, err := game.database.Exec("CREATE TABLE IF NOT EXISTS systemschema(systemid TEXT PRIMARY KEY, versionnumber INTEGER)");
+	_, err := game.database.Exec("CREATE TABLE IF NOT EXISTS systemschema(systemid TEXT PRIMARY KEY, versionnumber INTEGER)")
 	return err
 }
 
 func (game *Game) GetSchemaVersionNumber(systemid string) (int, error) {
+	var versionnumber int
+	err := game.database.QueryRow("SELECT versionnumber FROM systemschema WHERE systemid = ?", systemid).Scan(&versionnumber)
+	return versionnumber, err
 }
 
-func (game *Game) SetSchemaVersionNumber(systemid string, number int) error {
-
+func (game *Game) SetSchemaVersionNumber(systemid string, versionnumber int) error {
+	_, err := game.database.Exec("INSERT OR REPLACE INTO systemschema(systemid,versionnumber) VALUES(?, ?)", systemid, versionnumber)
+	return err
 }
