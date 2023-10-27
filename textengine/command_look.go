6c306679ed49e6fc937870227c7bca9e7b8d20fa
Benjamin Leskey <ben@benleskey.com>
2023-10-27 15:45:45 -0400

    More look refactoring.

--- textengine/command_look.go
+++ textengine/command_look.go
@@ -5,58 +5,22 @@ import (
 	"strings"
 )
 
-type lookDescriptor struct {
-	entity EntityRef
-	look_type string
-	look_desc string
-}
-
-func (entity EntityRef) getLooks() ([]lookDescriptor, error) {
-	var looks []lookDescriptor
-
-	rows, err := entity.game.database.Query("SELECT l.entityid, l.type, l.desc FROM entity_look l WHERE l.entityid IN (SELECT r.recieverid FROM entity_relationship r WHERE r.providerid IN (SELECT r2.providerid FROM entity_relationship r2 WHERE r2.recieverid = ? AND r2.alive = 1 AND r2.relationshipverb = 'contains') AND r.relationshipverb = 'contains' AND r.alive = 1) OR l.entityid IN (SELECT r.providerid FROM entity_relationship r WHERE r.alive = 1 AND r.relationshipverb = 'contains' AND r.recieverid = ?)", entity.id, entity.id)
-
-	if err != nil {
-		return looks, err
-	}
-
-	defer rows.Close()
-
-	for rows.Next() {
-		var look lookDescriptor
-		var entity_id string
-
-		if err := rows.Scan(&entity_id, &look.look_type, &look.look_desc); err != nil {
-			return looks, err
-		}
-
-		look.entity = entity.game.GetEntity(entity_id)
-
-		looks = append(looks, look)
-	}
-
-	if err = rows.Err(); err != nil {
-		return looks, err
-	}
-
-	return looks, nil
-}
-
 func CommandLookRegister(game *Game, commands *Commands) {
 	command := commands.Register("look", func(client *Client, args CommandInput) {
-		looks, err := client.entity.getLooks()
-		if err != nil {
-			client.SendAll(CommandOutput{}, args, err)
-			return
-		}
+		looks := client.entity.GetLooks()
 
 		var looksText []string
 
 		var output = make(CommandOutput)
 
-		for _, look := range looks {
+		for lookResult := range looks {
+			if lookResult.err != nil {
+				client.SendAll(CommandOutput{}, args, lookResult.err)
+				return
+			}
+			look := lookResult.look
 			looksText = append(looksText, fmt.Sprintf("%s", look.look_desc))
-			output[look.entity.id] = fmt.Sprintf("%q %q", look.look_type, look.look_desc)
+			output[look.entity.id] = fmt.Sprintf("%s %q %q", output[look.entity.id], look.look_type, look.look_desc)
 		}
 
 		output["text"] = strings.Join(looksText, "\n")
--- textengine/system_look.go
+++ textengine/system_look.go
@@ -1,5 +1,11 @@
 package textengine
 
+type LookDescriptor struct {
+	entity EntityRef
+	look_type string
+	look_desc string
+}
+
 func systemLookInitialize(system *System) error {
 	v, err := system.GetSchemaVersionNumber()
 	if err != nil {
@@ -26,4 +32,47 @@ func SystemLookRegister(game *Game) {
 func (entity *EntityRef) AddLook(looktype string, desc string) error {
 	_, err := entity.game.database.Exec("INSERT INTO entity_look(entityid, type, desc) VALUES (?, ?, ?)", entity.id, looktype, desc)
 	return err
+}
+
+type LookDescriptorResult struct {
+	look LookDescriptor
+	err error
+}
+
+func (entity EntityRef) GetLooks() (chan LookDescriptorResult) {
+	c := make(chan LookDescriptorResult)
+
+	go func() {
+
+		defer close(c)
+
+		rows, err := entity.game.database.Query("SELECT l.entityid, l.type, l.desc FROM entity_look l WHERE l.entityid = ?", entity.id)
+
+		if err != nil {
+			c <- LookDescriptorResult{LookDescriptor{}, err}
+			return
+		}
+
+		defer rows.Close()
+
+		for rows.Next() {
+			var look LookDescriptor
+			var entity_id string
+
+			if err := rows.Scan(&entity_id, &look.look_type, &look.look_desc); err != nil {
+				c <- LookDescriptorResult{LookDescriptor{}, err}
+				return
+			}
+
+			look.entity = entity.game.GetEntity(entity_id)
+
+			c <- LookDescriptorResult{look, nil}
+		}
+
+		if err = rows.Err(); err != nil {
+			c <- LookDescriptorResult{LookDescriptor{}, err}
+		}
+	}()
+
+	return c
 }
