8aee62f8106622185c8a4b64eb1d2a6c634d8442
Benjamin Leskey <ben@benleskey.com>
2025-11-29 09:47:46 -0500

    feat: Add JLine dependency for improved CLI line editing and enhance Client input handling

--- src/main/java/com/benleskey/textengine/cli/Client.java
+++ src/main/java/com/benleskey/textengine/cli/Client.java
@@ -1,39 +1,61 @@
 package com.benleskey.textengine.cli;
 
-import com.benleskey.textengine.Game;
-import com.benleskey.textengine.commands.CommandInput;
-import com.benleskey.textengine.commands.CommandOutput;
-import com.benleskey.textengine.util.Markup;
-import lombok.Builder;
-
-import java.util.Scanner;
+import java.io.IOException;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Flow;
 
+import org.jline.reader.EndOfFileException;
+import org.jline.reader.LineReader;
+import org.jline.reader.LineReaderBuilder;
+import org.jline.reader.UserInterruptException;
+import org.jline.terminal.Terminal;
+import org.jline.terminal.TerminalBuilder;
+
+import com.benleskey.textengine.Game;
+import com.benleskey.textengine.commands.CommandInput;
+import com.benleskey.textengine.commands.CommandOutput;
+import com.benleskey.textengine.exceptions.InternalException;
+import com.benleskey.textengine.util.Markup;
+
+import lombok.Builder;
+
 public class Client extends com.benleskey.textengine.Client {
-	private final Scanner scanner = new Scanner(System.in);
 	private final boolean apiDebug;
+
+	private Terminal terminal = null;
+	private LineReader reader = null;
 
 	@Builder
 	private Client(Game game, boolean apiDebug) {
 		this.game = game;
 		this.apiDebug = apiDebug;
+
+		try {
+			terminal = TerminalBuilder.builder().system(true).dumb(true).build();
+		} catch (IOException e) {
+			throw new InternalException("Failed to initialize terminal", e);
+		}
+		reader = LineReaderBuilder.builder().terminal(terminal).build();
 	}
 
 	@Override
 	public CommandInput waitForInput() {
-		System.out.print("> ");
+
+		String line = null;
+		try {
+			line = reader.readLine("> ");
+		} catch (UserInterruptException e) {
+			return waitForInput();
+		} catch (EndOfFileException e) {
+			return CommandInput.make(M_QUIT_FROM_CLIENT);
+		}
+
 		CommandInput toServer;
-		if (scanner.hasNextLine()) {
-			String line = scanner.nextLine();
-			if (line.trim().isEmpty()) {
-				// Recurse
-				return waitForInput();
-			} else {
-				toServer = game.inputLineToCommandInput(line);
-			}
+
+		if (line.trim().isEmpty()) {
+			return waitForInput();
 		} else {
-			toServer = CommandInput.make(M_QUIT_FROM_CLIENT);
+			toServer = game.inputLineToCommandInput(line);
 		}
 
 		if (apiDebug) {
@@ -89,5 +111,18 @@ public class Client extends com.benleskey.textengine.Client {
 		});
 
 		future.join();
+	}
+
+	@Override
+	public void quitFromServer() {
+		super.quitFromServer();
+		// Close the terminal if it was opened
+		if (terminal != null) {
+			try {
+				terminal.close();
+			} catch (IOException e) {
+				// Ignore
+			}
+		}
 	}
 }
